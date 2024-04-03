859cbac93bbfad3205e9ea8f9f8c892d19ce16aa
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-04-04 01:43:02 +0530

    added header

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
-  return <>React app</>;
+  return (
+    <>
+      <div className="App">
+       <Header/>
+      </div>
+    </>
+  );
 }
 
 export default App;
