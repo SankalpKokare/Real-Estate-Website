a30fc682dbb7b2c29b4034180e972cf59b5ff8e4
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-04-06 21:22:21 +0530

    Added content in value in left and right container

---
+++ my-react-app/src/Components/Value/Value.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+  AccordionItemState,
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import "./Value.css";
+
+function Value() {
+  return (
+    <section className="v-wrapper">
+      <div className="padding innerWidth flexCenter v-container">
+        {/* left side */}
+        <div className="v-left">
+          <div className="image-container">
+            <img src="./value.png" alt="" />
+          </div>
+        </div>
+
+        {/* Right side */}
+        <div className="flexColStart v-right">
+          <span className="orangeText">Our Value</span>
+          <span className="primaryText">Value We Give To You</span>
+          <span className="secondaryText">
+            We always ready to help by providing the best services for you.
+            <br />
+            We believe a good blace to live can make your life better
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Value;
