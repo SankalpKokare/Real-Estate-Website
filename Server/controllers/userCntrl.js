55f0c476c1051ce7284118a6552f9104e1e3da11
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-04-08 15:42:49 +0530

    added route to register new user

--- Server/controllers/userCntrl.js
+++ Server/controllers/userCntrl.js
@@ -5,5 +5,17 @@ export const createUser = asyncHandler(async (req, res) => {
     console.log("Create a user");
 
     let { email } = req.body;
-    console.log(email)
+    // Check if userexist
+    const userExist = await prisma.user.findUnique({ where: { email: email } })
+
+    if (!userExist) {
+
+        const user = await prisma.user.create({ data: req.body });
+        res.send({
+            message: "User created successfully",
+            user: user,
+        });
+    } else {
+        res.status(201).send({ message: "User already registered" });
+    }
 })
