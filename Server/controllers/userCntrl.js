967858fb57623913df21dc0804604380df0d5e51
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-04-08 18:50:07 +0530

    added route to get all booking by email

--- Server/controllers/userCntrl.js
+++ Server/controllers/userCntrl.js
@@ -52,3 +52,22 @@ export const bookVisit = asyncHandler(async (req, res) => {
         throw new Error(err.message)
     }
 })
+
+//function to get all booking of a user by email
+
+export const getAllBookings = asyncHandler(async (req, res) => {
+
+    const { email } = req.body;
+    try {
+        const bookings = await prisma.user.findUnique({
+            where: { email },
+            select: { bookedVisits: true }
+        })
+
+        res.status(200).send(bookings)
+
+    } catch (err) {
+        throw new Error(err.message);
+    }
+
+})
