b8c231235d0bfe4e8d21c537b3724af3cbcf1bdd
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-04-10 01:51:26 +0530

    added search bar in properties page

--- Client/my-react-app/src/pages/Properties/Properties.jsx
+++ Client/my-react-app/src/pages/Properties/Properties.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import SearchBar from "../../Components/SearchBar/SearchBar";
+import "./Properties.css";
 
 function Properties() {
-  return <div className="wrapper">Properties</div>;
+  return (
+    <div className="wrapper">
+      <div className="flexColCenter paddings innerWidth properties-container">
+        <SearchBar />
+      </div>
+    </div>
+  );
 }
 
 export default Properties;
