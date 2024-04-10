1e8ce8ee106a7b1dfb03b10df55870739c570f78
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-04-10 14:01:09 +0530

    get all properties data from server

--- Client/my-react-app/src/pages/Properties/Properties.jsx
+++ Client/my-react-app/src/pages/Properties/Properties.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import SearchBar from "../../Components/SearchBar/SearchBar";
 import "./Properties.css";
+import useProperties from "../../hooks/useProperties";
 
 function Properties() {
+  const { data, isError, isLoading } = useProperties();
+  console.log(data);
   return (
     <div className="wrapper">
       <div className="flexColCenter paddings innerWidth properties-container">
