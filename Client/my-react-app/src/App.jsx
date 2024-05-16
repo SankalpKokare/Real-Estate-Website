ae2ec7104e4919a17213baf85a3db0544db81388
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-05-16 23:26:43 +0530

    register user in database from auth0

--- Client/my-react-app/src/App.jsx
+++ Client/my-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import "./App.css";
 import { MantineProvider } from "@mantine/core";
 import Website from "./pages/Website.jsx";
@@ -10,10 +10,17 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Property from "./pages/Property/Property.jsx";
+import UserDetailContext from "./Context/UserDetailsContext.js";
 function App() {
   const queryClient = new QueryClient();
+  const [userDetails , setUserDetails] = useState({
+    favourites: [],
+    booking: [],
+    token: null
+  })
   return (
     <>
+    <UserDetailContext.Provider value={{userDetails , setUserDetails}}>
       <MantineProvider>
         <QueryClientProvider client={queryClient}>
           <BrowserRouter>
@@ -33,6 +40,7 @@ function App() {
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </MantineProvider>
+      </UserDetailContext.Provider>
     </>
   );
 }
